9a05eb3418fb399f1ab9bb88fae10b5b10c5ce89
Long Hoang <99398806+long20102004@users.noreply.github.com>
2025-01-23 18:08:10 +0700

    Update api-fetch.js

--- long4nihongo_nextjs/lib/api-fetch.js
+++ long4nihongo_nextjs/lib/api-fetch.js
@@ -1,5 +1,5 @@
 export async function apiFetch(url, options = {}) {
-  const baseUrl = "https://20.239.232.148:8443";
+  const baseUrl = "https://long4nihongo.online";
   const finalUrl = url.startsWith("http") ? url : `${baseUrl}/${url}`;
   const { method = "GET", ...restOptions } = options;
   const response = await fetch(finalUrl, {
