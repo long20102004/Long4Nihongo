40f6ad8605103c818a2dadf085149f1a2e7820a0
long20102004 <hoanghailonguyno@gmail.com>
2025-01-27 00:31:09 +0700

    fix localstorage

--- long4nihongo_nextjs/app/course-introduce/[id]/page.js
+++ long4nihongo_nextjs/app/course-introduce/[id]/page.js
@@ -67,7 +67,7 @@ export default function CoursePage({ params: paramsPromise }) {
   const [hadCourse, setHadCourse] = useState(false);
   const handleCheckout = () => {
     setChoosedCourse([course]);
-    localStorage.setItem("choosedCourses", course);
+    localStorage.setItem("choosedCourses", JSON.stringify(course));
     router.push("/checkout");
   };
   const [courses, setCourseData] = useState([]);
