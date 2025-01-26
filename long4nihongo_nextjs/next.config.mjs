df4c359db25b273a5dcd8e67811d3c82961e4289
Long Hoang <99398806+long20102004@users.noreply.github.com>
2025-01-26 14:29:37 +0700

    Update next.config.mjs

--- long4nihongo_nextjs/next.config.mjs
+++ long4nihongo_nextjs/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["img.vietqr.io", "github.com", "cdn.long4nihongo.online"],
+    domains: ["img.vietqr.io", "github.com", "cdn.longnihongo.com"],
   },
   // async rewrites() {
   //   return [
