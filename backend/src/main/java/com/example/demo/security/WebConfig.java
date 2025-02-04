e7404ccf07c922d0e32687bc04095b42acff9ee4
long20102004 <hoanghailonguyno@gmail.com>
2025-02-04 11:07:27 +0700

    fixed: multiprops for local and production, fix authentication to secure data

--- backend/src/main/java/com/example/demo/security/WebConfig.java
+++ backend/src/main/java/com/example/demo/security/WebConfig.java
@@ -1,18 +1,18 @@
-package com.example.demo.security;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://www.longnihongo.com", "https://api.longnihongo.com", "https://admin.longnihongo.com", "http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
-                .allowedHeaders("*")
-                .allowedMethods("*")
-                .allowCredentials(true);
-    }
-}
+//package com.example.demo.security;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("https://www.longnihongo.com", "https://api.longnihongo.com", "https://admin.longnihongo.com")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowedMethods("*")
+//                .allowCredentials(true);
+//    }
+//}
